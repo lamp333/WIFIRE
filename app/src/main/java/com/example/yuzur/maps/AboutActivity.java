53f6f9efe0c982acdfd0c565b3388f9c3c940e29
Jamie Zhang <jmzhang003@gmail.com>
2017-11-30 15:43:21 -0800

    added about activity

---
+++ app/src/main/java/com/example/yuzur/maps/AboutActivity.java
@@ -0,0 +1,124 @@
+package com.example.yuzur.maps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class AboutActivity extends AppCompatActivity {
+    private final int HOME = 0;
+    private final int TAKE_PICTURE = 1;
+    private final int SETTINGS = 2;
+    private final int ABOUT = 3;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] mNavMenuOptions;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mNavMenuOptions = getResources().getStringArray(R.array.nav_menu);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mNavMenuOptions));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("asdf", i + "");
+                switch (i) {
+                    case HOME:
+                        goHome();
+                        break;
+                    case TAKE_PICTURE:
+                        startPictureIntent();
+                        break;
+                    case SETTINGS:
+                        openSettings();
+                        break;
+                    case ABOUT:
+                        break;
+                }
+            }
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(getTitle().toString());
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startPictureIntent() {
+        Intent i = new Intent(this, CameraActivity.class);
+        /*i.putExtra("longitude", longitude);
+        i.putExtra("latitude", latitude);
+        i.putExtra("altitude", altitude);
+        i.putExtra("accuracy", accuracy);
+        i.putExtra("direction", azimuth);*/
+
+        startActivity(i);
+    }
+
+    private void openSettings() {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+    }
+
+    private void goHome(){
+        Log.d("asdf", "going home");
+        Intent i = new Intent(this, MapsActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+    }
+}
--- app/src/main/java/com/example/yuzur/maps/MapsActivity.java
+++ app/src/main/java/com/example/yuzur/maps/MapsActivity.java
@@ -59,6 +59,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private final int HOME = 0;
     private final int TAKE_PICTURE = 1;
     private final int SETTINGS = 2;
+    private final int ABOUT = 3;
 
     private GoogleMap mMap;
     private GoogleApiClient client;
@@ -129,6 +130,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     case SETTINGS:
                         openSettings();
                         break;
+                    case ABOUT:
+                        openAbout();
                 }
             }
         });
@@ -273,7 +276,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onLocationChanged(Location location) {
-
         lastLocation = location;
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         latitude = location.getLatitude();
@@ -387,8 +389,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         startActivity(i);
     }
 
-    public void openSettings() {
+    private void openSettings() {
         Intent i = new Intent(MapsActivity.this, SettingsActivity.class);
+        startActivity(i);
+    }
+
+    private void openAbout(){
+        Intent i = new Intent(MapsActivity.this, AboutActivity.class);
         startActivity(i);
     }
 
